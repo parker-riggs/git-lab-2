c80ec827479c5cdf7bc776581272ac5faf1c8117
Parker <pr881922@ohio.edu>
2023-01-25 19:59:46 -0500

    Hello!

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Parker Riggs
+ *        Date: 1 / 25 / 23
+ * Description: Git lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+cout << "Hello Git!!" << endl;
+    return 0;
+    
+}// main
